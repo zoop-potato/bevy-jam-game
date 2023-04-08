3c3e0e6a869d4436d39f23a6a8e8ee7654cf3c43
Kianuu <krezazadeh@hotmail.com>
2023-04-08 08:50:33 -0400

    Added background music on loop, updated visuals

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use bevy_kira_audio::prelude::*;
+use bevy_kira_audio::prelude::Audio; // Preventing naming conflict between kira_audio and regular bevy (both have Audio) 
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 use bevy_asset_loader::prelude::*;
@@ -8,11 +10,15 @@ use cauldron::CauldronPlugin;
 use ingredient::IngredientPlugin;
 use potion::PotionPlugin;
 use shelf::ShelfPlugin;
+//use audio::AudioPlugin;
+
 
 mod cauldron;
 mod ingredient;
 mod potion;
 mod shelf;
+mod audio;
+
 
 fn main() {
     App::new()
@@ -26,12 +32,19 @@ fn main() {
         .add_plugin(IngredientPlugin)
         .add_plugin(ShelfPlugin)
         .add_plugin(PotionPlugin)
+        .add_plugin(AudioPlugin)
         .add_startup_system(setup_camera)
+        .add_system(start_background_audio.on_startup())
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+// Simple method to loop the OST on startup with kira_audio ver 0.15.0 
+fn start_background_audio(assets: Res<AssetServer>, audio: Res<Audio>) { 
+    audio.play(assets.load("etntrack.ogg")).looped();
 }
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
